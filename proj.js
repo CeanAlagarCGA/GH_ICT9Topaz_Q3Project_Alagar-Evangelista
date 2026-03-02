6494ae7f171ac71e4163920d763214a89e8f70f5
CeanAlagarCGA <alagar.cf@obmontessori.edu.ph>
2026-03-02 20:59:23 +0800

    Add files via upload

---
+++ proj.js
@@ -0,0 +1,64 @@
+
+
+const profileInput = document.getElementById('profile');
+const profilePreview = document.getElementById('profilePreview');
+
+profilePreview.onclick = () => profileInput.click();
+
+profileInput.onchange = e => e.target.files[0] && (profilePreview.src = URL.createObjectURL(e.target.files[0]));
+
+
+const stations = [
+    "Baclaran Station",
+    "Monumento",
+    "5th Avenue",
+    "R. Papa",
+    "Abad Santos",
+    "Blumentritt",
+    "Tayuman",
+    "Bambang",
+    "Doroteo Jose",
+    "Carriedo",
+    "Central Terminal",
+    "United Nations",
+    "Pedro Gil",
+    "Quirino Avenue",
+    "Vito Cruz",
+    "Gil Puyat",
+    "Libertad",
+    "EDSA"
+];
+
+const pickupSelect = document.getElementById("pickup");
+const dropoffSelect = document.getElementById("dropoff");
+
+
+stations.forEach((station, index) => {
+    pickupSelect.innerHTML += `<option value="${index}">${station}</option>`;
+    dropoffSelect.innerHTML += `<option value="${index}">${station}</option>`;
+});
+
+function calculateFare() {
+
+    const baseFare = 50;
+    const perKMrate = 15;
+    const baseKM = 2;
+    let pickupIndex = parseInt(pickupSelect.value);
+    let dropoffIndex = parseInt(dropoffSelect.value);
+    let passengerType = document.getElementById("passengerType").value;
+    if (isNaN(pickupIndex) || isNaN(dropoffIndex) || pickupIndex === dropoffIndex) {
+        document.getElementById("result").innerHTML ="Please select valid stations.";
+    return;
+    }
+    let distance = Math.abs(dropoffIndex - pickupIndex);  
+    let totalFare = baseFare;
+    if (distance > baseKM) {
+        totalFare += (distance - baseKM) * perKMrate;
+    } 
+    if (passengerType === "discount") {
+        totalFare *= 0.80;
+    }
+    document.getElementById("result").innerHTML =
+        "Distance: " + distance + " km<br>" +
+        "Total Fare: &#8369;" + totalFare.toFixed(2);
+}
