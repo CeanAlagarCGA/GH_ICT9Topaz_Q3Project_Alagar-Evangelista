998a4130bf93985a61310d708e11db77106a2c19
CeanAlagarCGA <alagar.cf@obmontessori.edu.ph>
2026-03-03 13:13:20 +0800

    Update proj.js

--- proj.js
+++ proj.js
@@ -55,11 +55,3 @@ function calculateFare() {
 }
 
 
-
-const profileInput = document.getElementById('profile');
-const profilePreview = document.getElementById('profilePreview');
-
-profilePreview.onclick = () => profileInput.click();
-
-profileInput.onchange = e => e.target.files[0] && (profilePreview.src = URL.createObjectURL(e.target.files[0]));
-
